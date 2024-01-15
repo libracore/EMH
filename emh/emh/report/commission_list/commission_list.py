06a1b266ca3c2d68da80f9549eac7c2824713a03
Ivanios <ivan.lochbihler@libracore.com>
2024-01-15 16:11:03 +0100

    new sql_query

--- emh/emh/report/commission_list/commission_list.py
+++ emh/emh/report/commission_list/commission_list.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, libracore and contributors
+# Copyright (c) 2013, libracore and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -26,21 +26,44 @@ def get_columns():
 
 
 def get_data(filters):
+    #original query, with real datas from the invoices
+    # ~ sql_query = """
+        # ~ SELECT
+            # ~ `customer` AS `customer`,
+            # ~ `customer_name` AS `customer_name`,
+            # ~ `posting_date` AS `invoice_date`,
+            # ~ `name` AS `invoice_no`,
+            # ~ `sales_partner` AS `sales_partner`,
+            # ~ `base_net_total` AS `turnover`,
+            # ~ `commission_rate` AS `commission_rate`,
+            # ~ `commission` AS `total_commission`
+        # ~ FROM `tabSales Invoice`
+        # ~ WHERE `posting_date` BETWEEN '{from_date}' AND '{to_date}'
+            # ~ AND `commission_rate` > 0
+            # ~ AND `docstatus` = 1
+        # ~ ORDER BY `customer` DESC
+    # ~ """.format(from_date=filters.from_date, to_date=filters.to_date)
+    
+    #Temporary query which works retroactive, has to be replaced by original query, because it dous only show actual commission rate (from the client)
     sql_query = """
         SELECT
-            `customer` AS `customer`,
-            `customer_name` AS `customer_name`,
-            `posting_date` AS `invoice_date`,
-            `name` AS `invoice_no`,
-            `sales_partner` AS `sales_partner`,
-            `base_net_total` AS `turnover`,
-            `commission_rate` AS `commission_rate`,
-            `commission` AS `total_commission`
-        FROM `tabSales Invoice`
-        WHERE `posting_date` BETWEEN '{from_date}' AND '{to_date}'
-            AND `commission_rate` > 0
-            AND docstatus = 1
-        ORDER BY `customer` DESC
+			`invoice`.`name` AS `invoice_no`,
+			`invoice`.`base_net_total` AS `turnover`,
+			`invoice`.`customer_name`,
+			`invoice`.`customer`,
+			`invoice`.`posting_date` AS `invoice_date`,
+			`tabCustomer`.`default_sales_partner` AS `sales_partner`,
+			`tabCustomer`.`default_commission_rate` AS `commission_rate`,
+			SUM(`item`.`amount`/100*`tabCustomer`.`default_commission_rate`) AS `total_commission`
+        FROM `tabSales Invoice` AS `invoice`
+        INNER JOIN `tabCustomer` ON `invoice`.`customer` = `tabCustomer`.`name`
+        INNER JOIN `tabSales Invoice Item` AS `item` ON `item`.`parent` = `invoice`.`name`
+        WHERE `item`.`item_code` = "1000"
+        AND `tabCustomer`.`default_sales_partner` IS NOT NULL
+        AND `invoice`.`docstatus` = 1
+        AND `invoice`.`posting_date` BETWEEN '{from_date}' AND '{to_date}'
+        GROUP BY `invoice_no`
+        ORDER BY `invoice_no` DESC
     """.format(from_date=filters.from_date, to_date=filters.to_date)
    
     data = frappe.db.sql(sql_query, as_dict=True)
