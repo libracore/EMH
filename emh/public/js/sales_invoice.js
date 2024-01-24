99e1f649847a2b4a5ccbb494cf5aabb384d5a0a9
lasalesi <larsericmueller@gmail.com>
2024-01-24 11:15:04 +0100

    Remove obsolete file

--- emh/public/js/sales_invoice.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2021-2023, libracore AG and contributors
-// For license information, please see license.txt
-
-//~ frappe.ui.form.on('Sales Invoice', {
-    //~ before_save: function(frm) {
-    //~ }
-//~ });
